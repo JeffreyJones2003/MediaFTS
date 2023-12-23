f9172f3c69028982cb87e2781fca6883378ab1d5
Jeffrey Jones S <135623574+JeffreyJones2003@users.noreply.github.com>
2023-12-23 22:13:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
+
+function redirectToTarget() {
+    // Use window.location to redirect to the target HTML document
+    window.location.href = "services.html";
+}
+
+
+
+
+
+
+
